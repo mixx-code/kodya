c1c9662c707e908021f25fea0b31ae4e2f6e6597
mixx-code <rfebriansyah385@gmail.com>
2025-12-27 01:56:08 +0700

    perbaikan middleware

--- app/middleware.ts
+++ app/middleware.ts
@@ -60,13 +60,7 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
     matcher: [
-        /*
-         * Match all request paths except for the ones starting with:
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
-         * - public folder
-         */
-        '/((?!_next/static|_next/image|favicon.ico|public/).*)',
+        '/((?!_next/static|_next/image|favicon.ico|public/|auth/callback).*)',
+        //                           tambahkan ini ^^^^^^^^^^^^^^
     ],
 }
